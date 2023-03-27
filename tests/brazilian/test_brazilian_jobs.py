38836ab8efdcc18a70c24c77898cdd1078c60b1e
Gabriel Puttin <gabrielputtin@hotmail.com>
2023-03-27 15:13:26 -0300

    Feat: unit test for read_brazilian_file

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,9 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
+
+path = 'tests/mocks/brazilians_jobs.csv'
+english_jobs = read_brazilian_file(path)
 
 
 def test_brazilian_jobs():
-    pass
+    'If brazilian_jobs returns correct value'
+    assert read_brazilian_file(path) == english_jobs
